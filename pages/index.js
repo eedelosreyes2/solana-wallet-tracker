bcd26bc144f0a760c0a53d8bd544b5905ffefe4c
eedelosreyes2 <delosreyes.elijah@gmail.com>
2022-06-07 22:17:27 -0700

    initial frontend

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="text-white bg-slate-900 h-screen">
+    <div className="text-white bg-slate-900 h-screen relative">
       <main>
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </main>
 
-      <footer></footer>
+      <footer className="absolute bottom-0 w-full flex justify-center py-5">
+        Created by Elijah
+      </footer>
     </div>
   );
 }
