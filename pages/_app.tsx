8997f2fd0d45937eb1acb2cd240533f8798df388
eedelosreyes2 <delosreyes.elijah@gmail.com>
2022-06-09 23:25:40 -0700

    favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ const App = ({ Component, pageProps }: AppProps) => {
           name="description"
           content="Track the activity of a Solana Wallet"
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/SOL.jpg" />
       </Head>
       <Component {...pageProps} />
     </>
